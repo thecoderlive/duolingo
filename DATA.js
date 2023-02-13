7a88e0c6caca74968a9c91f4abdd8080050ac9e7
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-13 19:30:38 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,49 @@
+export const item = {
+    "id": "59ab17f7-edfd-4550-b7c0-927793abb210",
+    "duolingo_title": "collection",
+    "about_lesson": "Style on plant wonder standard system. Police happen get experience. None my cup research consider teacher impact.",
+    "duolingo_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/360.png",
+    "power_ups": "Trouble example area star girl daughter matter education. Could try also as those. Seat culture reflect significant shake everything article.",
+    "power_up_list": [
+        {
+            "id": "7558b96b-fbc4-4ccc-af9e-00e44e781bb2",
+            "power_up_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/20.png",
+            "power_up_title": "Policy per grow manager message. Well pattern easy last meet national serve pick.",
+            "about_power_up": "Dream shoulder enjoy two. Inside move identify enter. Power reach conference this health.",
+            "gem_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/159.png",
+            "power_count": 81
+        },
+        {
+            "id": "c2b92e07-a949-48df-ad56-0d94d1b4633e",
+            "power_up_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/108.png",
+            "power_up_title": "Religious discover by must. Ahead focus American discover.",
+            "about_power_up": "Pay speech admit garden. Produce community low officer most successful few.",
+            "gem_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/73.png",
+            "power_count": 5559
+        },
+        {
+            "id": "399fba2a-b3e5-4589-b2be-ef589cd286ab",
+            "power_up_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/8.png",
+            "power_up_title": "Dinner person find continue degree. Option heavy foreign news blood capital quite their.",
+            "about_power_up": "Design friend others serve.",
+            "gem_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/66.png",
+            "power_count": 1804
+        },
+        {
+            "id": "9adc6ee2-c5ce-4c71-9e38-3fc58853a3ef",
+            "power_up_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/238.png",
+            "power_up_title": "Leg economy southern manager statement fund. Most able learn mouth. Prove listen relationship meeting.",
+            "about_power_up": "Head participant like down seem these. Hope thousand including throw miss. Improve garden business TV field of lose.",
+            "gem_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/348.png",
+            "power_count": 7324
+        },
+        {
+            "id": "7c862705-8719-400e-92a9-91ff6183bb11",
+            "power_up_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/60.png",
+            "power_up_title": "Increase rest almost economic. Consumer practice dark but attorney rise successful. Traditional simple box lot help any response hear.",
+            "about_power_up": "Congress career once sing sense. Ten item he Mr effect reflect adult.",
+            "gem_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/368.png",
+            "power_count": 4216
+        }
+    ]
+}
